fb5592f9dff70923d75995514890c3076fc5baee
holgerkurtz <hk@holgerkurtz.de>
2022-12-06 20:19:12 +0100

    changed prompt

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ print(text.__dict__)
 new_ai_text = text.create_text(tokens=500)
 print(new_ai_text)
 
-with open("ai_prompt.txt", 'a+') as file:
+with open("ai_prompt.txt", 'w+') as file:
     file.write(new_ai_text)
 
-# n = Newsletter(ai_text=new_ai_text)
-# r = n.send(n.create_id())
+n = Newsletter(ai_text=new_ai_text)
+r = n.send(n.create_id())
