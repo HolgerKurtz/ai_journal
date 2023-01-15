b1079473e0a3130140bf367696b82cd8b8721818
holgerkurtz <hk@holgerkurtz.de>
2023-01-15 12:12:46 +0100

    test

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ def get_weekday():
     return week_day
 
 def send():
-    abstract = get_news_from_nyt(0, "home").get("abstract")
-    ai_prompt = new_prompt(abstract, date.today())
-    text = TextGen(prompt=ai_prompt)
+    # abstract = get_news_from_nyt(0, "home").get("abstract")
+    # ai_prompt = new_prompt(abstract, date.today())
+    beethoven_prompt = "Write a letter from Ludwig van Beethoven. Be specific and detail oriented:\n" + choose_letters(number=6)
+    
+    text = TextGen(prompt=beethoven_prompt)
     print(text.__dict__)
     new_ai_text = text.create_text(tokens=500)
     print(new_ai_text)
