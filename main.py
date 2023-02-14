28166b2270deaf796b703424b76022dbd7bc0ed1
KulturData <kulturdata@gmail.com>
2023-02-14 06:54:10 +0000

    now only mittwochs

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def send():
     # abstract = get_news_from_nyt(0, "home").get("abstract")
     # ai_prompt = new_prompt(abstract, date.today())
     beethoven_prompt = "Write a letter from Ludwig van Beethoven. Be specific and detail oriented:\n" + choose_letters(number=6)
-    
+
     text = TextGen(prompt=beethoven_prompt)
     print(text.__dict__)
     new_ai_text = text.create_text(tokens=500)
@@ -26,8 +26,5 @@ def send():
 
 if __name__ == "__main__":
     wd = get_weekday()
-    if wd == 3:
-        print("mittwoch") # for weekly use
-        send()
-    else:
+    if wd == 3: # mittwoch
         send()
