f8dc72bfea06422471bbb8b61f877618a8bdcb17
holgerkurtz <hk@holgerkurtz.de>
2023-04-20 19:56:15 +0100

    now with gpt4

--- ai.py
+++ ai.py
@@ -14,26 +14,21 @@ class TextGen:
         self.variable = variable
 
     def create_text(self, tokens=256):
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=self.prompt,
-            temperature=0.3,
-            max_tokens=tokens,
-            top_p=1,
-            frequency_penalty=0.8,
-            presence_penalty=0.7,
-            stop="---"
-        )
-
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                    {"role": "user", "content": self.prompt}
+                ]
+            )
         # print the first response
-        ai_response = response.choices[0].text
+        ai_response = response['choices'][0]['message']['content']
         print(response)
         return ai_response
 
 
 if __name__ == "__main__":
     from choose_random_letters import choose_letters
-    beethoven_prompt = "Write a letter from Ludwig van Beethoven. Be specific and detail oriented:\n" + choose_letters(number=6)
+    beethoven_prompt = f"You are BeethovenGPT. You come up with new letters from Ludwig van Beethoven. You write in the style of Beethoven himself and you keep the given formatting. Here are some real letters from him as a reference:\n{choose_letters(number=6)}"
     text = TextGen(prompt=beethoven_prompt)
     ai_text = text.create_text()
     print(ai_text)
--- main.py
+++ main.py
@@ -14,17 +14,21 @@ def get_weekday():
 def send():
     # abstract = get_news_from_nyt(0, "home").get("abstract")
     # ai_prompt = new_prompt(abstract, date.today())
-    beethoven_prompt = "Write a letter from Ludwig van Beethoven. Be specific and detail oriented:\n" + choose_letters(number=6)
-
+    beethoven_prompt = f"You are BeethovenGPT. You come up with new letters from Ludwig van Beethoven. You write in the style of Beethoven himself but you add what he might have thought about new and upcoming artists from today. Here are some real letters from him as a reference:\n{choose_letters(number=6)}"
     text = TextGen(prompt=beethoven_prompt)
-    print(text.__dict__)
+    # print(text.__dict__)
     new_ai_text = text.create_text(tokens=500)
-    print(new_ai_text)
-
+    # print(new_ai_text)
+    print("1. Text wurde erstellt.")
+    print("2. E-Mail wird erstellt.")
     n = Newsletter(ai_text=new_ai_text)
-    r = n.send(n.create_id()) # n.send_test(n.create_id())
+    r = n.send(n.create_id()) # real
+    # n.send_test(n.create_id()) # test
+    print("3. E-Mail versandt")
 
 if __name__ == "__main__":
     wd = get_weekday()
     if wd == 3: # mittwoch
+        send()
+    else:
         send()
